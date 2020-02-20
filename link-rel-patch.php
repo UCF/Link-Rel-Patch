d19c778dc032b41414da51be1c3189cd64ae4daa
Cadie Stockman <cadie.brown@gmail.com>
2020-02-20 15:45:05 -0500

    Move from main-site-theme fixes

--- link-rel-patch.php
+++ link-rel-patch.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Link Rel Patch
 Description: A WordPress plugin that filters the rel values that are automatically added on links that are set to open in a new tab. The only rel value that is added to these links on page update or publish is `noopener`.
-Version: 0.0.0
+Version: 1.0.0
 Author: UCF Web Communications
 License: GPL3
 GitHub Plugin URI: UCF/Link-Rel-Patch
@@ -20,9 +20,8 @@ if ( ! defined( 'WPINC' ) ) {
  * Sets the rel value upon post publish and update for links that are
  * set to open in a new tab to only `noopener`.
  *
- * @since v3.3.7
+ * @since v1.0.0
  * @author Cadie Stockman
- * @param string $rel The rel values
  */
 function replace_rel_value() {
 	return 'noopener';
